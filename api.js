71898d713c9da95871c21954563ff1a340172e20
yurij842shvets <yurij2011shvets@gmail.com>
2025-11-15 00:43:31 +0200

    finished api js home and movies pages

--- api.js
+++ api.js
@@ -1,27 +1,39 @@
-const API_KEY = '89c70d0401c8471ace31f88b910df57c'
-const URL = 'https://api.themoviedb.org/3'
+const API_KEY = "89c70d0401c8471ace31f88b910df57c";
+const URL = "https://api.themoviedb.org/3";
 
 export async function fetchTrending() {
-    const res = await fetch(`${URL}/trending/get-trending?api_key=${API_KEY}`)
-    return res.json()
+  const res = await fetch(`${URL}/trending/movie/day?api_key=${API_KEY}`);
+  const data = await res.json();
+  return data.results || [];
 }
 
 export async function fetchMovies(query) {
-    const res = await fetch(`${URL}/query=${query}/search/search-movies?api_key=${API_KEY}`)
-    return res.json()
+  const res = await fetch(
+    `${URL}/search/movie?api_key=${API_KEY}&query=${query}`
+  );
+  const data = await res.json();
+  return data.results || [];
 }
 
 export async function fetchMovieDetails(movieID) {
-    const res = await fetch(`${URL}/movies/${movieID}/get-movie-details?api_key=${API_KEY}`)
-    return res.json()
+  const res = await fetch(
+    `${URL}/movie/${movieID}?api_key=${API_KEY}`
+  );
+  return res.json() || [];
 }
 
 export async function fetchMovieCredits(movieID) {
-    const res = await fetch(`${URL}/${movieID}/movies/get-movie-credits?api_key=${API_KEY}`)
-    return res.json()
+  const res = await fetch(
+    `${URL}/movie/${movieID}/credits?api_key=${API_KEY}`
+  );
+  const data = await res.json();
+  return data.cast || [];
 }
 
 export async function fetchMovieReviews(movieID) {
-    const res = await fetch(`${URL}/${movieID}/movies/get-movie-reviews ?api_key=${API_KEY}`)
-    return res.json()
+  const res = await fetch(
+    `${URL}/movie/${movieID}/reviews?api_key=${API_KEY}`
+  );
+  const data = await res.json();
+  return data.results || [];
 }
