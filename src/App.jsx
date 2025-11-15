5bacf9482b0f79aa61d9555ca9d6b7e8d4a396d0
yurij842shvets <yurij2011shvets@gmail.com>
2025-11-15 23:58:09 +0200

    started moviedetails

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
-
         </Routes>
       </BrowserRouter>
     </>
